c5fa7e68110ec286e940964292a8f2d79b3d74fc
Jackson Pires <jack@jack.com>
2020-05-04 13:56:33 +0000

    Fixing the assets and the web console

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,5 @@ module TimeToAnswer
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    # permite web-console
-    config.web_console.whitelisted_ips = '10.0.2.2'
   end
 end
