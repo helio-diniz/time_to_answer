09a33d1a90cb28bbecb397d2748e77c7535a5716
Jackson Pires <jack@jack.com>
2020-05-22 12:55:12 +0000

    Force the Yarn Install

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,3 +50,16 @@ set :default_env, {
    PATH: '$HOME/.npm/:$PATH',
    NODE_ENVIRONMENT: 'production'
 }
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
